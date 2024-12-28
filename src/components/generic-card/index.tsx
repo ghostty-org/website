f4178ff2deef342ff5810586df39d932dafdfbaf
Liam Canetti <git@liamcanetti.co.uk>
2024-12-28 12:54:33 +0000

    feat(card): update to allow image & only pad content

--- src/components/generic-card/index.tsx
+++ src/components/generic-card/index.tsx
@@ -1,9 +1,18 @@
 import { SpacingProp } from "@/types/style";
 import { H2, P } from "../text";
 import s from "./GenericCard.module.css";
+import Image from "next/image";
+
+interface ImageProps {
+  src: string;
+  alt: string;
+  width: number | `${number}`;
+  height: number | `${number}`;
+}
 
 interface GenericCardProps {
   title: string;
+  image?: ImageProps;
   description: string;
   padding?: SpacingProp;
   children?: React.ReactNode;
@@ -11,15 +20,19 @@ interface GenericCardProps {
 
 export default function GenericCard({
   title,
+  image,
   padding = "56px",
   description,
   children,
 }: GenericCardProps) {
   return (
-    <div className={s.genericCard} style={{ padding }}>
-      <H2 className={s.title}>{title}</H2>
-      <P className={s.description}>{description}</P>
-      {children ? <div className={s.children}>{children}</div> : null}
+    <div className={s.genericCard}>
+      {image ? <Image {...image} /> : null}
+      <div style={{ padding }}>
+        <H2 className={s.title}>{title}</H2>
+        <P className={s.description}>{description}</P>
+        {children ? <div className={s.children}>{children}</div> : null}
+      </div>
     </div>
   );
 }
