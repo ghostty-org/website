ca7ba823f371bd682eff377814a025fd39a6d783
Mitchell Hashimoto <m@mitchellh.com>
2025-07-02 14:48:10 -0700

    Show a warning banner at the top if we're on the tip branch

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    GIT_COMMIT_REF: process.env.VERCEL_GIT_COMMIT_REF || '',
+  },
 };
 
 export default nextConfig;
